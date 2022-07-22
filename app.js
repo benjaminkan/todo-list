8bd1a5298f3b4fb5355d025649a1bc4ac17f1e82
benjaminkan <50194133+benjaminkan@users.noreply.github.com>
2022-07-22 19:24:49 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+const addForm = document.querySelector(".add");
+const list = document.querySelector(".todos");
+const search = document.querySelector(".search input");
+
+const generateTemplate = todo => {
+    const html = `
+    <li>
+     <span>${todo}</span>
+     <i class="far fa-trash-alt delete"></i>
+    </li>
+    `;
+    list.innerHTML += html;
+};
+
+const filterTodos = (term) => {
+
+    Array.from(list.children)
+        .filter(todo => !todo.textContent.toLowerCase().includes(term))
+        .forEach(todo => todo.classList.add('filtered'));
+
+    Array.from(list.children)
+        .filter(todo => todo.textContent.toLowerCase().includes(term))
+        .forEach(todo => todo.classList.remove('filtered'));
+
+};
+
+// add todos
+addForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const todo = addForm.add.value.trim();
+
+    if (todo.length > 0 && todo.length < 25) {
+        generateTemplate(todo);
+        addForm.reset();
+    }
+});
+
+// delete todos
+list.addEventListener('click', e => {
+    if (e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
+    }
+});
+
+// filter todos
+search.addEventListener('keyup', () => {
+    const term = search.value.trim().toLowerCase();
+    filterTodos(term);
+});
